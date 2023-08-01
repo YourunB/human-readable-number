e0cd899b9155e70f213cfccf5a80efdd447cf3ef
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-08-01 11:37:50 +0300

    feat: add counting up to 99

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+    let result = "";
+    number = String(number);
+
+    arr1 = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+    arr2 = [,,"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+
+      if (number.length === 1 ) result = result + arr1[number];
+      if (number.length === 2 && number <= 19) result = result + arr1[number];
+
+      if (number.length === 2 && number >= 20) {
+        result = result + arr2[number[0]];
+        if (number[1] != 0) result = result + " " + arr1[number[1]];
+      }
+
+      if (number.length === 3) {
+        result = result + arr1[number[0]] + " hundred ";
+      }
+      
+
+  return result;
 }
